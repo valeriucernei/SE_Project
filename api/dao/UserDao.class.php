84eb70dc993c0a706bf5e82ff48081446e321a5c
valeriucernei <cernei.valeriu99@gmail.com>
2021-04-20 17:31:25 +0200

    created UserDao, get_user_by_emai/username

---
+++ api/dao/UserDao.class.php
@@ -0,0 +1,20 @@
+<?php
+require_once dirname(__FILE__)."/BaseDao.class.php";
+
+class UserDao extends BaseDao{
+
+    public function __construct(){
+        parent::__construct("users");
+    }
+
+    public function get_user_by_email($email){
+        return $this->query_unique("SELECT * FROM users WHERE email = :email",
+                                    ["email" => $email]);
+    }
+
+    public function get_user_by_username($username){
+      return $this->query_unique("SELECT * FROM users WHERE username =
+                                  :username", ["username" => $username]);
+    }
+
+}
