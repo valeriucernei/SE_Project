e5fb5e556df5089e10aa8020eb980494136689cb
valeriucernei <cernei.valeriu99@gmail.com>
2021-04-25 15:17:08 +0200

    added get_photo_id and get_ads_photos to PhotosDao

---
+++ api/dao/PhotosDao.class.php
@@ -0,0 +1,20 @@
+<?php
+require_once dirname(__FILE__)."/BaseDao.class.php";
+
+class PhotosDao extends BaseDao{
+
+    public function __construct(){
+        parent::__construct("photos");
+    }
+
+    public function get_photo_id($token){
+        return $this->query_unique("SELECT id FROM photos WHERE
+                                    name = :token", ["token" => $token]);
+    }
+
+    public function get_ads_photos($id){
+        return $this->query("SELECT * FROM photos WHERE ad_id = :id",
+                            ["id" => $id]);
+    }
+
+}
