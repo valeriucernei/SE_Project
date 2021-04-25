559ca77aae1d80e1c8f299f997427ac45bb6f89f
nicolae.topala <nicolae.topala13@gmail.com>
2021-04-25 18:05:38 +0200

    added services and BaseService

---
+++ api/services/BaseService.class.php
@@ -0,0 +1,29 @@
+<?php
+
+class BaseService{
+    protected $dao;
+
+    public function get_by_id($id){
+        return $this->dao->get_by_id($id);
+    }
+
+    public function add($data){
+        return $this->dao->add($data);
+    }
+
+    public function delete($token){
+        return $this->dao->delete($token);
+    }
+
+    public function update($id, $data){
+        $this->dao->update($id, $data);
+        return $this->dao->get_by_id($id);
+    }
+
+    public function checkEmail($email) {
+         $find1 = strpos($email, '@');
+         $find2 = strpos($email, '.');
+         return ($find1 !== false && $find2 !== false && $find2 > $find1);
+    }
+
+}
