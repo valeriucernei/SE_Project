c6cce9e7ce3bfcb111f1ab171320ac2805afd200
valeriucernei <cernei.valeriu99@gmail.com>
2021-04-28 17:48:23 +0200

    added confirm, login and forgot to UserDao

--- api/services/UserService.class.php
+++ api/services/UserService.class.php
@@ -66,4 +66,68 @@ class UserService extends BaseService{
         return $user;
     }
 
+    public function confirm($token){
+        $user = $this->dao->get_user_by_token($token);
+        if(!isset($user['id']))
+            throw new Exception("Invalid token.");
+
+        $this->dao->update($user['id'], [
+        "status" => "ACTIVE",
+        "token" => NULL
+        ]);
+
+        $this->smtpClient->send_confirmed_email($user);
+
+        return $user;
+    }
+
+    public function login($user){
+        if(parent::checkEmail($user['login']))
+            $db_user = $this->dao->get_user_by_email($user['login']);
+        else
+            $db_user = $this->dao->get_user_by_username($user['login']);
+
+        if(!isset($db_user['id']))
+            throw new Exception("User doesn't exist.", 400);
+        if($db_user['status'] != 'ACTIVE')
+            throw new Exception("Your account has not been yet activated,
+                                  or is blocked.", 400);
+        if(md5($user['pass']) != $db_user['pass'])
+            throw new Exception("You have entered a wrong password.", 400);
+
+        return $db_user;
+    }
+
+    public function forgot($user){
+        $db_user = $this->dao->get_user_by_email($user['email']);
+        if(!isset($db_user))
+            throw new Exception("User doesn't exist.", 400);
+        if($db_user['status'] != 'ACTIVE')
+            throw new Exception("You account has not been yet activated, or is blocked.", 400);
+
+        $time = strtotime(date(config::DATA_FORMAT)) - strtotime($db_user['token_created_at']);
+        if( $time < 300 )
+          throw new Exception("A new token can be generated in ".(300-$time)." seconds!", 400);
+
+        $db_user = parent::update($db_user['id'], [
+            'token' => md5(random_bytes(16)),
+            'token_created_at' => date(Config::DATA_FORMAT)]);
+
+        $this->smtpClient->send_recovery_email($db_user);
+    }
+
+    public function reset($user){
+        $db_user = $this->dao->get_user_by_token($user['token']);
+        if(!isset($db_user['id']))
+            throw new Exception("Invalid token.");
+        if(strtotime(date(config::DATA_FORMAT)) - strtotime($db_user['token_created_at']) > 3600)
+            throw new Exception("Reset token expired. Generate a new one.", 400);
+
+        $this->dao->update($db_user['id'], [
+            "pass" => MD5($user['pass']),
+            "token" => null
+        ]);
+        return $db_user;
+    }
+
 }
